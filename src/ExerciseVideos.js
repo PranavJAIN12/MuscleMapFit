24713f3fff93649ba3c3975a24f6e0acaeb0cb5b
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-07-03 17:28:43 +0530

    ui improvement in video section

--- src/ExerciseVideos.js
+++ src/ExerciseVideos.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import './ExerciseVideo.css'
+import yt from './components/Images/icons8-youtube-120.png'
 export default function ExerciseVideos({ exerciseVideos = [], name }) {
   return (
     <div>
       {/* <p>heee</p> */}
-      <h3>Watch {name} videos</h3>
+      <h3 className='text-center'>Watch {name} videos</h3>
       {exerciseVideos.length > 0 ? (
         <div className='exercise-video-data'>
           {exerciseVideos.slice(0,5).map((item, index) => (
@@ -16,7 +17,8 @@ export default function ExerciseVideos({ exerciseVideos = [], name }) {
               target='_blank'
               rel='noreferrer'
             >
-              <img src={item.video.thumbnails[0].url} alt='data' />
+              <img className='mainimg' src={item.video.thumbnails[0].url} alt='data'  />
+              <img className='yt' src={yt}/>
             </a>
           ))}
         </div>
